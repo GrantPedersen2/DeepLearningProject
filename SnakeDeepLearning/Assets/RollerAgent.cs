8ba2dfa42e5974f9f0b5cc322de1f4f98f6743b8
GrantPedersen2 <grantpedersen2@live.com>
2018-11-13 19:42:57 -0700

    Basic training setup environment tutorial

---
+++ SnakeDeepLearning/Assets/RollerAgent.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RollerAgent : Agent
+{
+    // Use this for initialization
+    Rigidbody rBody;
+    public Transform Target;
+    public float speed = 10f;
+    private float previousDistance = float.MaxValue;
+
+    void Start ()
+    {
+        rBody = GetComponent<Rigidbody>();	
+	}
+
+    public override void AgentReset()
+    {
+       if(transform.position.y < -1.0)
+        {
+            transform.position = Vector3.zero;
+            rBody.angularVelocity = Vector3.zero;
+            rBody.velocity = Vector3.zero;
+        }
+       else
+        {
+            Target.position = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+        }
+    }
+
+    public override void CollectObservations()
+    {
+        // Calculate relative position
+        Vector3 relativePosition = Target.position - this.transform.position;
+
+        // Relative position
+        AddVectorObs(relativePosition.x / 5);
+        AddVectorObs(relativePosition.z / 5);
+
+        // Distance to edges of platform
+        AddVectorObs((this.transform.position.x + 5) / 5);
+        AddVectorObs((this.transform.position.x - 5) / 5);
+        AddVectorObs((this.transform.position.z + 5) / 5);
+        AddVectorObs((this.transform.position.z - 5) / 5);
+
+        // Agent velocity
+        AddVectorObs(rBody.velocity.x / 5);
+        AddVectorObs(rBody.velocity.z / 5);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.position, Target.position);
+        
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            AddReward(1.0f);
+            Done();
+        }
+
+        // Time penalty
+        AddReward(-0.05f);
+
+        // Fell off platform
+        if (this.transform.position.y < -1.0)
+        {
+            AddReward(-1.0f);
+            Done();
+        }
+
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rBody.AddForce(controlSignal * speed);
+    }
+}
